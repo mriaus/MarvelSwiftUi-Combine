2591455fd38a7b3eceffe425748f212902ac67f1
mriaus <55255215+mriaus@users.noreply.github.com>
2023-11-19 22:20:13 +0100

    Added watchOS build

---
+++ WatchOS Watch App/WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSApp.swift
+//  WatchOS Watch App
+//
+//  Created by Marcos on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
